e1ca376781810a76e5a1fd6324de29f4d2349761
agrosjean <agrosjean007@gmail.com>
2021-05-15 22:27:06 -0400

    Fixing my timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 var check = 0;
-var timeDisplEl = $("#time-display");
-var hourEl = $("#hour");
+var timeDispEl = $("#time-display");
+
 
 
 
 
 
 function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
     console.log(typeof rightnow);
-    timeDisplayEl.text(rightNow);
+    timeDispEl.text(rightNow);
     var present = rightNow.substring(16, 18);
     var check = parseInt(present);
     console.log("This is check value ", check);
@@ -19,14 +19,9 @@ function displayTime() {
 }
 
 function checkPresent(present) {
-    console.lFog("Value of Present: " + present);
+    console.log("Value of Present: " + present);
 
 }
 
-function save() {
-    // event listener when click on text box it lets them enter their appointements
-    // save-btn.on('click, write()){
-    // document.textContent
-}
 
 setInterval(displayTime, 1000);
